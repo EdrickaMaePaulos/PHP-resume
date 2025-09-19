5abc6870af125340bc9222d111dc682f6eb5df3f
EdrickaMaePaulos <edrickapaulos@gmail.com>
2025-09-19 14:20:51 +0800

    without log-in & register

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "paulos_cv";
+$user = "postgres";
+$pass = "100013433675001";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
